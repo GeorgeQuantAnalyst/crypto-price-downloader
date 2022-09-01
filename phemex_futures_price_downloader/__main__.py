b4d814e93d00d68942d342e0b3db1d7a3801e3b9
jirka <jiri.work@outlook.com>
2022-09-01 10:20:40 +0200

    Implemented project version 1.0.0

---
+++ phemex_futures_price_downloader/__main__.py
@@ -0,0 +1,33 @@
+import logging.config
+
+import ccxt
+import pandas as pd
+
+from phemex_futures_price_downloader import __version__
+
+__logo__ = """
+---------------------------------------------------------------------
+phemex-futures-price-downloader {}
+---------------------------------------------------------------------
+""".format(__version__.__version__)
+
+LOGGER_CONFIG_FILE_PATH = "logger.conf"
+
+# Init
+logging.config.fileConfig(fname=LOGGER_CONFIG_FILE_PATH, disable_existing_loggers=False)
+
+if __name__ == '__main__':
+    logging.info(__logo__)
+    logging.info("Load dataset assets.csv")
+    assets = pd.read_csv("data/assets.csv")
+    exchange = ccxt.phemex()
+
+    logging.info("Replace suffix and prefix in tickers")
+    assets["Ticker"] = assets["Ticker"].str.replace("PERP", "")
+    assets["Ticker"] = assets["Ticker"].str.replace("100", "u100")
+
+    logging.info("Fetch last price for tickers")
+    assets["LastPrice"] = assets["Ticker"].apply(lambda x: exchange.fetch_ticker(x)["last"])
+
+    logging.info("Save result to csv")
+    assets.to_csv("data/assets_with_price.csv")
---
+++ run.sh
@@ -0,0 +1,3 @@
+source venv/bin/activate
+python3 -m phemex_futures_price_downloader
+libreoffice data/assets_with_price.csv
