aaad818f76113e00efd2804d551685f71e9e9187
jirka <jiri.caga@proton.me>
2022-10-13 14:33:34 +0200

    refactoring: - changed log path - converted if elif to match case - formatted config file

--- crypto_price_downloader/__main__.py
+++ crypto_price_downloader/__main__.py
@@ -20,20 +20,19 @@ logging.info(__logo__)
 config = load_config(CONFIG_FILE_PATH)
 
 if __name__ == '__main__':
-    crypto_price_downloader = CryptoPriceDownloader(config["RateExceedDelaySeconds"])
+    crypto_price_downloader = CryptoPriceDownloader(config["base"]["rateExceedDelaySeconds"])
     exchange = sys.argv[1]
 
-    if exchange == "PhemexFutures":
-        crypto_price_downloader.download_phemex_futures_price()
-
-    if exchange == "KucoinSpot":
-        crypto_price_downloader.download_kucoin_spot_price()
-
-    if exchange == "BinanceSpot":
-        crypto_price_downloader.download_binance_spot_price()
-
-    if exchange == "Okx":
-        crypto_price_downloader.download_okx_spot_price()
-
-    if exchange == "SimpleFx":
-        crypto_price_downloader.download_simple_fx_cfd_price()
+    match exchange:
+        case "PhemexFutures":
+            crypto_price_downloader.download_phemex_futures_price()
+        case "KucoinSpot":
+            crypto_price_downloader.download_kucoin_spot_price()
+        case "BinanceSpot":
+            crypto_price_downloader.download_binance_spot_price()
+        case "Okx":
+            crypto_price_downloader.download_okx_spot_price()
+        case "SimpleFx":
+            crypto_price_downloader.download_simple_fx_cfd_price()
+        case _:
+            raise Exception("Not supported exchange - [}".format(exchange))
