d6f6a0655b8f8c16ec2e558dde6c30b91056d19c
jirka <jiri.caga@proton.me>
2022-11-11 14:04:30 +0100

    Added create log folder in prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -4,3 +4,9 @@ set -e
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+LOG_FOLDER="$HOME/App/log"
+APP_LOG_FOLDER="$HOME/App/log/crypto-place-trades"
+
+mkdir -p ${LOG_FOLDER}
+mkdir -p ${APP_LOG_FOLDER}
